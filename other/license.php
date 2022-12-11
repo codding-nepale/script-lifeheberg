5b6088a567a2f52a34e083cff74aa23fd9cc3d18
codding-nepale <79444135+codding-nepale@users.noreply.github.com>
2022-12-11 20:40:04 +0100

    Create license.php

---
+++ other/license.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * LICENSE:
+ * Please provide your Proxmox VPS Module For WHMCS license below
+ * update 11/12/2022 at 20:40
+ */
+$proxmox_vps_licensekey="ModulesGarden_ProxmoxVPS_813316fe031f6d";
